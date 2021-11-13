c50f39152c14c2fa6e59d50c9c017f41d151c144
Peter Wolf <pewobox@Peters-Air.fritz.box>
2021-11-13 12:10:50 +0100

    fetched news article from RAPIDAPI and map it to seperate compon

---
+++ components/Articles.tsx
@@ -0,0 +1,7 @@
+export default function Articles(props){
+    return (
+        <li>
+            {props.title}
+        </li>
+    )
+}
--- components/FetchMovieData.tsx
+++ components/FetchMovieData.tsx
@@ -1,5 +1,42 @@
-export default function FetchMovieData(){
-    return(
-        <></>
+import React, {useState, useEffect} from 'react';
+import Articles from'./Articles'
+
+export default function FetchMovieData() {
+
+    const [newsData, setNewsData] = useState();
+
+    useEffect(() => {
+        fetch("https://free-news.p.rapidapi.com/v1/search?q=Elon%20Musk&lang=en&page=1", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "free-news.p.rapidapi.com",
+                "x-rapidapi-key": "f2f2dfce82mshcb64156556ea80ap1968b8jsn5e6669fe879d"
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+
+                setNewsData(data.articles)
+                console.log(newsData)
+
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    }, []);
+
+    // @ts-ignore
+    const ListOfArticle = newsData.map((article)=> <Articles title={article.title} key={article.id}/>)
+
+    return (
+        <section>
+        <div>
+
+
+        </div>
+        <ul>
+        {ListOfArticle}
+        </ul>
+        </section>
     )
 }
